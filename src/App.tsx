6003cc15dc3b74d55879160f140e12b3b60cbef6
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-12-03 15:25:46 +0530

    Started Navbar Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Men from "./pages/Men";
+import Women from "./pages/Women";
+import Kids from "./pages/Kids";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
+    <div>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/kids" element={<Kids />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,47 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { FaSearch } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-around">
+      <div>
+        <img src={logo} className="w-[70px]" />
+      </div>
+
+      <div className="flex gap-2 font-bold">
+        <NavLink to="/">
+          <p>Home</p>
+        </NavLink>
+        <NavLink to="/men">
+          <p>Men</p>
+        </NavLink>
+        <NavLink to="/women">
+          <p>Women</p>
+        </NavLink>
+        <NavLink to="/kids">
+          <p>Kids</p>
+        </NavLink>
+        <NavLink to="/about">
+          <p>About Us</p>
+        </NavLink>
+        <NavLink to="/contact">
+          <p>Contact Us</p>
+        </NavLink>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          placeholder="Search Here"
+          className="border-2 rounded-lg"
+        />
+        <FaSearch className="text-xl" />
+        <CgProfile className="text-xl" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
